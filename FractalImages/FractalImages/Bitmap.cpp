7223ad383c2e6ffcd811d957625fd55936e5fe00
Amit Fainshtein <amitfain1@gmail.com>
2021-09-17 17:27:18 +0300

    feat(Bitmap): add bitmap class

---
+++ FractalImages/FractalImages/Bitmap.cpp
@@ -0,0 +1,11 @@
+#include "Bitmap.h"
+
+namespace myproj {
+	Bitmap::Bitmap(int width, int height) : _width(width), _height(height) {}
+
+	void Bitmap::setPixel(int x, int y, uint8_t red, uint8_t green, uint8_t blue){}
+
+	bool Bitmap::write(std::string filename) {
+		return false;
+	}
+}
---
+++ FractalImages/FractalImages/Bitmap.h
@@ -0,0 +1,22 @@
+#ifndef BITMAP_H_
+#define BITMAP_H_
+
+#include <string>
+#include <cstdint>
+
+namespace myproj {
+	class Bitmap
+	{
+	private:
+		int _width{ 0 };
+		int _height{ 0 };
+
+	public:
+		Bitmap(int width, int height);
+		void setPixel(int x, int y, uint8_t red, uint8_t green, uint8_t blue);
+		bool write(std::string filename);
+	};
+}
+
+#endif // !BITMAP_H_
+
