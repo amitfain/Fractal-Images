cd8e77b15dab16084ff47e90b727bcfbff86c201
Amit Fainshtein <amitfain1@gmail.com>
2021-09-17 16:31:45 +0300

    feat(BitmapFileHeaders): add bitmapFileHeaders object

--- BitmapFileHeaders/BitmapFileHeaders/BitmapFileHeader.h
+++ BitmapFileHeaders/BitmapFileHeaders/BitmapFileHeader.h
@@ -1,6 +1,14 @@
-#pragma once
+#pragma pack(2)
 #ifndef BITMAPFILEHEADER_H_
 #define BITMAPFILEHEADER_H_
 
+#include <cstdint>
+
+struct BitmapFileHeader {
+	char header[2]{ 'B', 'M' };
+	int32_t fileSize;
+	int32_t reserved{ 0 };
+	int_fast32_t dataOffset;
+};
 
 #endif // !BITMAPFILEHEADER_H_
